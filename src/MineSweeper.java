099fc8764f62aa815a50cca9e7b3007c7b17a3fe
Yaroslav Andrushchenko <yarxslav1109@gmail.com>
2022-07-27 17:01:57 +0300

    Ranges and Coord classes added

--- src/MineSweeper.java
+++ src/MineSweeper.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 
 import sweeper.Box;
+import sweeper.Coord;
+import sweeper.Ranges;
 
 public class MineSweeper extends JFrame {
     private JPanel panel;
@@ -14,6 +16,7 @@ public class MineSweeper extends JFrame {
     }
 
     private MineSweeper() {
+        Ranges.setSize(new Coord(COLS, ROWS));
         setImages();
         initPanel();
         initFrame();
@@ -26,11 +29,13 @@ public class MineSweeper extends JFrame {
                 super.paintComponent(g);
 
                 for (Box box : Box.values()) {
-                    g.drawImage((Image)box.image, box.ordinal() * IMAGE_SIZE, 0, this);
+                    Coord coord = new Coord(box.ordinal() * IMAGE_SIZE, 0);
+                    g.drawImage((Image) box.image, coord.getX(), coord.getY(), this);
                 }
             }
         };
-        panel.setPreferredSize(new Dimension(COLS * IMAGE_SIZE, ROWS * IMAGE_SIZE));
+        panel.setPreferredSize(new Dimension(Ranges.getSize().getX() * IMAGE_SIZE,
+                Ranges.getSize().getY() * IMAGE_SIZE));
         add(panel);
     }
 
@@ -41,6 +46,7 @@ public class MineSweeper extends JFrame {
         setLocationRelativeTo(null);
         setResizable(false);
         setVisible(true);
+        setIconImage(getImage("icon"));
     }
 
     private void setImages() {
---
+++ src/sweeper/Ranges.java
@@ -0,0 +1,14 @@
+package sweeper;
+
+public class Ranges {
+    private static Coord size;
+
+    public static void setSize(Coord newSize) {
+        size = newSize;
+    }
+
+    public static Coord getSize() {
+        return size;
+    }
+
+}
